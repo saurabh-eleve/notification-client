d39bdd911f1df245b65325b075e82380512853ff
saurabh.eleve <saurabh@eleve.co.in>
2020-09-10 15:32:05 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = io('https://rtnserver.herokuapp.com');
+
+export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      notifications: []
+    }
+
+  }
+
+  componentDidMount = () => {
+    let _ = this;
+    socket.on('connect', () => {
+      console.log('Client is Connected');
+    });
+    // emitting to get notifications
+    socket.emit('notifications');
+    // listeing to get notitications
+    socket.on('allnotifications', function(val){
+      _.handleNotifications(val);
+    });
+
+    socket.on('notificationCreated', function(val){
+      _.handleCreatedNotifications(val);
+    });
+  }
+
+  handleNotifications = (notifications) => {
+    toast(notifications[notifications.length - 1].name);
+    this.setState(prevState => ({
+      notifications
+    }))
+  }
+
+  handleCreatedNotifications = notification => {
+    toast.success(notification.name);
+    this.setState(prevState => ({
+      notifications: [...prevState.notifications, notification]
+    }))
+  }
+
+  createNotification = () => {
+    socket.emit('createNotifications');
+  }
+
+  render = () => {
+    const { notifications } = this.state;
+    return (
+      <div className="App">
+        <p>You have received {notifications.length} notifications.</p>
+        <button className="button" onClick={this.createNotification}>Create Notification</button>
+        {
+          notifications.map((n, i) => {
+            return (
+              <div key={n.id} className="notification-item">
+                {n.name}
+              </div>
+            )
+          })
+        }
+        <ToastContainer />
+      </div>
+    );
+  }
 }
-
-export default App;
